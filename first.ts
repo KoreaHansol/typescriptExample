70d18c524b2ba40c3f325087af24023f7cda648b
KoreaHansol <hskimaa@hanmail.net>
2023-06-12 14:11:23 +0900

    타입스크립트 공부

---
+++ first.ts
@@ -0,0 +1,10 @@
+const a: number = 5;
+const b: string = 'abc';
+const c: boolean = true;
+const d: null = null;
+const e: undefined = undefined;
+const f: any = 'asjdio';
+
+// function add(x: number, y: number): number { return x + y }
+// const add: (x: number, y: number) => number = (x, y) => x + y;
+// const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 };
